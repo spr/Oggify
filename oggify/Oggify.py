14ec6ace690fdcbe1621c7870cdf123df665fc1e
Scott Paul Robertson <spr@scottr.org>
2008-01-13 20:43:20 -0800

    functional style

--- oggify/Oggify.py
+++ oggify/Oggify.py
@@ -5,51 +5,54 @@ This is the worker module of Oggify. All common functions live here.
 
 import os, os.path
 
+def _src_parse(arg, dirname, fnames):
+    src_ext, dst_ext, encode, dirs = arg
+    for file in fnames:
+        if os.path.islink(file):
+            continue # TODO: do something with this
+        if os.path.isdir(file):
+            dirs[('/'.join((dirname[2:], file)))] = None
+            continue
+        if file.endswith(src_ext):
+            src_fname = '/'.join((dirname[2:], file))
+            dst_fname = '.'.join(src_fname.split('.')[:-1] + [dst_ext])
+            encode[src_fname] = dst_fname
+
 def _walk_src_tree(root, src_ext, dst_ext):
     org_dir = os.getcwd()
     os.chdir(root)
     symlink_dirs = []
-    directories = {}
+    dirs = {}
     encode = {}
-    for current, subdirs, files in os.walk('.'):
-        for subdir in subdirs:
-            directories['/'.join((current[2:], subdir))] = None
-            if os.path.islink(subdir):
-                symlink_dirs.append(subdir)
-        for file in files:
-            if file.endswith(src_ext):
-                encode['/'.join((current[2:], file))] = \
-                        '/'.join((current[2:],
-                            '.'.join(file.split('.')[:-1] + [dst_ext])))
+    os.path.walk('.', _src_parse, (src_ext, dst_ext, encode, dirs))
     os.chdir(org_dir)
-    return (encode, directories)
+    return (encode, dirs)
 
-def _compare_dst_tree(root, extension, encode, directories, src_extension):
+def _dst_parse(arg, dirname, fnames):
+    src_ext, dst_ext, encode, dirs = arg
+    for file in fnames:
+        if os.path.islink(file):
+            continue # TODO: do I care?
+        if (os.path.isdir(file)
+                and '/'.join((dirname[2:], file)) not in dirs):
+            fname.remove(file)
+            continue # TODO: Add to purge!
+        dst_fname = '/'.join((dirname[2:], file))
+        src_fname_eq = '.'.join(src_fname.split('.')[:-1] + [src_ext])
+        if src_fname_eq in encode and not file.endswith(dst_ext):
+            # TODO: limited purge!
+            pass
+        elif src_fname_eq in encode and file.endswith(dst_ext):
+            # TODO: reencode
+            pass
+
+def _compare_dst_tree(root, dst_ext, encode, dirs, src_ext):
     org_dir = os.getcwd()
     os.chdir(root)
     reencode = {}
     limited_purge = []
     purge = []
-    for current, subdirs, files in os.walk('.'):
-        dont_walk = []
-        for subdir in subdirs:
-            if '/'.join((current[2:], subdir)) not in directories:
-                purge.append('/'.join((current[2:], subdir)))
-                dont_walk.append(subdir)
-        for dir in dont_walk:
-            subdirs.remove(dir)
-
-        for file in files:
-            src_eq = '/'.join((current[2:],
-                '.'.join(file.split('.')[:-1] + [src_extension])))
-            if src_eq in encode:
-                if not file.endswith(extension):
-                    limited_purge.append('/'.join((current[2:], file)))
-                else:
-                    reencode[src_eq] = '/'.join((current[2:], file))
-                    del encode[src_eq]
-            else:
-                purge.append('/'.join((current[2:], file)))
+    os.path.walk('.', _dst_parse, (src_ext, dst_ext, encode, dirs))
     os.chdir(org_dir)
     return (encode, reencode, limited_purge, purge)
 
