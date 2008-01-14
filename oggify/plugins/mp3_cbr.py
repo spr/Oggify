df90749009472e050863e676eca27ae90844f38c
Scott Paul Robertson <spr@scottr.org>
2008-01-13 20:49:04 -0800

    Licensing to GPLv2

--- oggify/plugins/mp3_cbr.py
+++ oggify/plugins/mp3_cbr.py
@@ -1,3 +1,22 @@
+# oggify.plugins.mp3_cbr - MP3 - CBR encoder plugin for Oggify
+# Copyright (c) 2007 Scott Paul Robertson (spr@scottr.org)
+#
+# This is part of Oggify (http://scottr.org/oggify/)
+#
+# Oggify is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version
+# 
+# Oggify is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Oggify; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 from oggify import plugins
 from tag_wrapper import tag
 from subprocess import Popen, STDOUT
--- setup.py
+++ setup.py
@@ -4,11 +4,27 @@ from distutils.core import setup
 
 setup(name='Oggify',
       version='2.0.0b1',
-      description='Oggify - audio conversion tool and libraries',
+      description='audio conversion tool for music library conversion',
+      long_description="""Oggify provides the tools needed to convert an
+      audio library from one format to another. Orginally designed to handle
+      the author's need of FLAC -> Ogg Vorbis. It ships with support for FLAC
+      as the source format, and Ogg Vorbis and MP3 as output formats. Requires
+      flac, vorbis-tools, and lame for full operation. Supports other formats
+      through a plugin system.""",
       author='Scott Paul Robertson',
       author_email='spr@scottr.org',
       url='http://scottr.org/oggify/',
       packages=['oggify', 'oggify.plugins'],
       requires=['tag_wrapper'],
       scripts=['bin/oggify'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: Advanced End Users',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python',
+          'Topic :: Multimedia :: Sound/Audio :: Conversion',
+          ]
      )
