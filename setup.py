03bb03ebc6e21b1f5d0cbed1d3ff0fe77e660963
Scott Paul Robertson <spr@scottr.org>
2008-01-13 20:48:43 -0800

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='Oggify',
-      version='2.0.0a1',
+      version='2.0.0b1',
       description='Oggify - audio conversion tool and libraries',
       author='Scott Paul Robertson',
       author_email='spr@scottr.org',
