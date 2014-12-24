942a26e79f75c93c5c2a7bea575908039742abee
gbcox <gbcox@bzb.us>
2014-12-23 22:11:19 -0800

    Updated FSF address

--- oggify/plugins/aac.py
+++ oggify/plugins/aac.py
@@ -2,20 +2,20 @@
 # Copyright (c) 2008 Scott Paul Robertson (spr@scottr.org)
 #
 # This is part of Oggify (http://scottr.org/oggify/)
-#
-# Oggify is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version
 # 
-# Oggify is distributed in the hope that it will be useful,
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
-# along with Oggify; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 import tempfile, os, os.path
 from tag_wrapper import tag
